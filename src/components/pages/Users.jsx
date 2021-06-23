85eaf1c9a00b26a87c132d8ab0f70db1a8bc3bd1
kamami <69190381+kamami-src@users.noreply.github.com>
2021-06-23 13:28:41 +0900

    Initial commit

---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organism/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `ユーザ${val}`,
+    image: "https://source.unsplash.com/cfIcTiopin4",
+    email: "niconico@gmail.com",
+    tel: "080-2525-2525",
+    company: {
+      name: "にこ株式会社"
+    },
+    website: "https://nico.jp"
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => {
+          return <UserCard key={user.id} user={user} />;
+        })}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
